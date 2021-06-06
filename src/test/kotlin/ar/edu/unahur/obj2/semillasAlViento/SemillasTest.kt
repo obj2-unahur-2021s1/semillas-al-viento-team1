dc5b42ab94c7a918091a25698f8a60c9cc14325f
Brenda Denhoff <brenda.denhoff@estudiantes.unahur.edu.ar>
2021-06-06 17:14:19 -0300

    Test requerimiento de parcelas

--- src/test/kotlin/ar/edu/unahur/obj2/semillasAlViento/SemillasTest.kt
+++ src/test/kotlin/ar/edu/unahur/obj2/semillasAlViento/SemillasTest.kt
@@ -1,13 +1,14 @@
 package ar.edu.unahur.obj2.semillasAlViento
 
 import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.booleans.shouldBeFalse
 import io.kotest.matchers.shouldBe
 class SemillasTest : DescribeSpec({
     describe("Un planta")
     {
-        val plantaDeMenta=Menta(3,2.60)
-        val plantaSojaComun=Soja(2020,3.60,true)
-        val plantaSojaTransgenica=Soja(2019,4.6,false)
+        val plantaDeMenta = Menta(3, 2.60)
+        val plantaSojaComun = Soja(2020, 3.60, true)
+        val plantaSojaTransgenica = Soja(2019, 4.6, false)
         it("Si dan semillas")
         {
             plantaDeMenta.daSemillas().shouldBe(true)
@@ -20,13 +21,30 @@ class SemillasTest : DescribeSpec({
             plantaSojaComun.esFuerte().shouldBe(true)
             plantaSojaTransgenica.esFuerte().shouldBe(false)
         }
+        describe("parcela") {
+            val parcela1 = Parcela(5, 4, 10)
+            val parcela2 = Parcela(3, 4, 4)
+
+            parcela1.plantar(plantaDeMenta)
+            parcela1.plantar(plantaSojaComun)
+            parcela2.plantar(plantaSojaTransgenica)
+            parcela2.plantar(plantaSojaComun)
+
+            it("superficie") {
+                parcela1.superficie().shouldBe(20)
+                parcela2.superficie().shouldBe(12)
+            }
+            it("cantidad maxima de plantas") {
+                parcela1.cantidadMaximaPlantas().shouldBe(4)
+                parcela2.cantidadMaximaPlantas().shouldBe(8)
+            }
+            
+            it("tiene complicaciones") { //No existe la funcion para el test
+                parcela1.tieneComplicaciones().shouldBeFalse()
+                parcela1.plantar(plantaSojaTransgenica)
+                parcela1.tieneComplicaciones().shouldBeTrue()
+                parcela2.tieneComplicaciones().shouldBeTrue()
+            }
+        }
     }
-
-
-
-
-
-
-
-
 })
